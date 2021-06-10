94de20f57be3b1407016fa55b1a129a1a007f6e4
Gustavo Amaral <guga.adr@gmail.com>
2021-06-10 05:06:35 -0300

    Moving constants & Drawning class to movement file

--- src/PVPGame.java
+++ src/PVPGame.java
@@ -2,71 +2,20 @@ import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.*;
 import javax.swing.*;
-import java.io.*;
-import java.rmi.server.Skeleton;
-
-import javax.imageio.*;
 
 class pvpGame extends JFrame {
-  final int AttSkl1 = 0;
-  final int AttSkl2 = 1;
-  final int DeadSkl1 = 2;
-  final int DeadSkl2 = 3;
-  final int RelaxSkl = 4;
-  final int WalkSkl1 = 5;
-  final int WalkSkl2 = 6;
-  final int WalkSkl3 = 7;
-  final int WalkSkl4 = 8;
 
-  Image Skeleton[] = new Image[9];
-  //Image SwordKnight[] = new Image[6];
-  //Image Unicorn[] = new Image[6];
-  
-  Drawning drawning = new Drawning();
-
-  class Drawning extends JPanel {
-
-    Drawning() {
-      try {
-        setPreferredSize(new Dimension(1000, 600));
-        Skeleton[AttSkl1] = ImageIO.read(new File("../img/Skeleton-Sprites/Attacking1-Skeleton.png"));
-        Skeleton[AttSkl2] = ImageIO.read(new File("../img/Skeleton-Sprites/Attacking2-Skeleton.png"));
-        Skeleton[DeadSkl1] = ImageIO.read(new File("../img/Skeleton-Sprites/Dead1-Skeleton.png"));
-        Skeleton[DeadSkl2] = ImageIO.read(new File("../img/Skeleton-Sprites/Dead2-Skeleton.png"));
-        Skeleton[RelaxSkl] = ImageIO.read(new File("../img/Skeleton-Sprites/Relaxed-Skeleton.png"));
-        Skeleton[WalkSkl1] = ImageIO.read(new File("../img/Skeleton-Sprites/Walking1-Skeleton.png"));
-        Skeleton[WalkSkl2] = ImageIO.read(new File("../img/Skeleton-Sprites/Walking2-Skeleton.png"));
-        Skeleton[WalkSkl3] = ImageIO.read(new File("../img/Skeleton-Sprites/Walking3-Skeleton.png"));
-        Skeleton[WalkSkl4] = ImageIO.read(new File("../img/Skeleton-Sprites/Walking4-Skeleton.png"));
-      } catch (IOException e) {
-        JOptionPane.showMessageDialog(this, "The image cannot be loaded!\n" + e, "Error", JOptionPane.ERROR_MESSAGE);
-        System.exit(1);
-      }
-    }
-
-    public void paintComponent(Graphics g) {
-      super.paintComponent(g);
-      //g.drawImage(img, 0, 0, this);
-    //   g.drawImage(Skeleton[AttSkl1], 0, 0, this);
-    //   g.drawImage(Skeleton[AttSkl2], 50, 0, this);
-    //   g.drawImage(Skeleton[DeadSkl1], 100, 0, this);
-    //   g.drawImage(Skeleton[DeadSkl2], 150, 0, this);
-    g.drawImage(Skeleton[RelaxSkl], 200, 0, this);
-    //   g.drawImage(Skeleton[WalkSkl1], 250, 0, this);
-    //   g.drawImage(Skeleton[WalkSkl2], 300, 0, this);
-    //   g.drawImage(Skeleton[WalkSkl3], 380, 0, this);
-    //   g.drawImage(Skeleton[WalkSkl4], 420, 0, this);
-      Toolkit.getDefaultToolkit().sync();
-    }
-  }
+    //Declares a new Instance of movement to Access Inner Classes
+    movement m = new movement();
+    
 
   pvpGame() {
     super("PVP GAME");
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-    add(drawning);
+    add(m.draw);
     pack();
     setVisible(true);
-    movement m = new movement();
+    
     addKeyListener(new KeyAdapter() {
       public void keyPressed(KeyEvent e) {
         int c = e.getKeyCode();
--- src/movement.java
+++ src/movement.java
@@ -1,9 +1,69 @@
+import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.*;
+import javax.swing.*;
+import java.io.*;
+import javax.imageio.*;
 
 public class movement {
-    public void skelMov(int c) {
-        if (c == KeyEvent.VK_RIGHT) {
+
+    final int AttSkl1 = 0;
+    final int AttSkl2 = 1;
+    final int DeadSkl1 = 2;
+    final int DeadSkl2 = 3;
+    final int RelaxSkl = 4;
+    final int WalkSkl1 = 5;
+    final int WalkSkl2 = 6;
+    final int WalkSkl3 = 7;
+    final int WalkSkl4 = 8;
+  
+    Image Skeleton[] = new Image[9];
+    //Image SwordKnight[] = new Image[6];
+    //Image Unicorn[] = new Image[6];
+  
+    public class Drawning extends JPanel {
+  
+      Drawning() {
+        try {
+          setPreferredSize(new Dimension(1000, 600));
+          Skeleton[AttSkl1] = ImageIO.read(new File("../img/Skeleton-Sprites/Attacking1-Skeleton.png"));
+          Skeleton[AttSkl2] = ImageIO.read(new File("../img/Skeleton-Sprites/Attacking2-Skeleton.png"));
+          Skeleton[DeadSkl1] = ImageIO.read(new File("../img/Skeleton-Sprites/Dead1-Skeleton.png"));
+          Skeleton[DeadSkl2] = ImageIO.read(new File("../img/Skeleton-Sprites/Dead2-Skeleton.png"));
+          Skeleton[RelaxSkl] = ImageIO.read(new File("../img/Skeleton-Sprites/Relaxed-Skeleton.png"));
+          Skeleton[WalkSkl1] = ImageIO.read(new File("../img/Skeleton-Sprites/Walking1-Skeleton.png"));
+          Skeleton[WalkSkl2] = ImageIO.read(new File("../img/Skeleton-Sprites/Walking2-Skeleton.png"));
+          Skeleton[WalkSkl3] = ImageIO.read(new File("../img/Skeleton-Sprites/Walking3-Skeleton.png"));
+          Skeleton[WalkSkl4] = ImageIO.read(new File("../img/Skeleton-Sprites/Walking4-Skeleton.png"));
+        } catch (IOException e) {
+          JOptionPane.showMessageDialog(this, "The image cannot be loaded!\n" + e, "Error", JOptionPane.ERROR_MESSAGE);
+          System.exit(1);
+        }
+      }
+  
+      public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        //g.drawImage(img, 0, 0, this);
+      //   g.drawImage(Skeleton[AttSkl1], 0, 0, this);
+      //   g.drawImage(Skeleton[AttSkl2], 50, 0, this);
+      //   g.drawImage(Skeleton[DeadSkl1], 100, 0, this);
+      //   g.drawImage(Skeleton[DeadSkl2], 150, 0, this);
+      g.drawImage(Skeleton[RelaxSkl], 20, 0, this);
+      //   g.drawImage(Skeleton[WalkSkl1], 250, 0, this);
+      //   g.drawImage(Skeleton[WalkSkl2], 300, 0, this);
+      //   g.drawImage(Skeleton[WalkSkl3], 380, 0, this);
+      //   g.drawImage(Skeleton[WalkSkl4], 420, 0, this);
+        Toolkit.getDefaultToolkit().sync();
+      }
+    }
+
+    //Declares a new Instance of Drawning Class to be used on pvpGame.
+    Drawning draw = new Drawning();
+
+    public void skelMov(int move) {
+        if (move == KeyEvent.VK_RIGHT) {
             
         }
     }
+    
 }
